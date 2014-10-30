5365036f80372a5507ee2ca5140e1457fffa03af
Emil Istrup Karlsmose <emilistrupkarlsmose@gmail.com>
2014-10-30 13:48:12 +0100

    made some methods abstract

--- src/freeWarOnTerror/Card.java
+++ src/freeWarOnTerror/Card.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package freeWarOnTerror;
 
 /**
@@ -19,9 +14,7 @@ public class Card {
         this.alignment = alignment;
     }
 
-    public void play() {
-
-    }
+    public abstract void play();
 
     public Boolean getPlayable() {
         return true;
