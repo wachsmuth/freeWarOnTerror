4c38fb6422758e3f357ffe3576b0f134d36aea30
Emil Istrup Karlsmose <emilistrupkarlsmose@gmail.com>
2014-10-30 12:27:16 +0100

    added subclass for Iran

---
+++ src/freeWarOnTerror/CountryIran.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package freeWarOnTerror;
+
+/**
+ *
+ * @author Emil
+ */
+public class CountryIran extends Country {
+    
+    int governance = 2;
+        public CountryIran(String name) {
+        super(name);
+        noLongerNeedsTesting();
+    }
+    
+}
