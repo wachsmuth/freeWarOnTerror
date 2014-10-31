635d5094bdc7c6f87f51b8e5e7230a578a0cede8
Emil Istrup Karlsmose <emilistrupkarlsmose@gmail.com>
2014-10-31 01:37:06 +0100

    Alignment: Neutral > US

--- src/freeWarOnTerror/cards/Sanctions.java
+++ src/freeWarOnTerror/cards/Sanctions.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2014 Emil
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package freeWarOnTerror.cards;
 
@@ -17,7 +28,7 @@ public class Sanctions extends Card {
     Game game;
 
     public Sanctions(Game game) {
-        super(1, 1, true);
+        super(1, 2, true);
         this.game = game;
     }
     
