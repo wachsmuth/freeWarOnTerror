7a2ff6a89b7e8eeff6e7e6c8f0e8f5ca8f841afd
Emil Istrup Karlsmose <emilistrupkarlsmose@gmail.com>
2014-10-31 09:07:39 +0100

    added Plot class

---
+++ src/freeWarOnTerror/Plot.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2014 Emil
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package freeWarOnTerror;
+
+/**
+ *
+ * @author Emil
+ */
+public class Plot {
+    
+    private final int type; //Type 1, 2, 3 = strength; type 4: WMD
+    
+    public Plot (int type){
+        this.type = type;
+    }
+    
+    
+    public int getType(){
+        return type;
+    }
+}
