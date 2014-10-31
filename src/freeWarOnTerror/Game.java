7588dddc382d1f21613973c0db1b397f3c7780ca
Emil Istrup Karlsmose <emilistrupkarlsmose@gmail.com>
2014-10-31 01:58:41 +0100

    added get & set for funding & prestige

--- src/freeWarOnTerror/Game.java
+++ src/freeWarOnTerror/Game.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2014 Emil
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package freeWarOnTerror;
 
@@ -13,60 +24,69 @@ import java.util.Arrays;
  * @author Emil
  */
 public class Game {
-    
+
     private int prestige = 7;
     private int funding = 9;
     private final Boolean[] markedCards = new Boolean[22];
     private final ArrayList<Country> countries = new ArrayList<>();
     private Deck drawPile = new Deck();
     private Deck discardPile = new Deck();
-    
-    public Game(){
+
+    public Game() {
         Arrays.fill(markedCards, false);
     }
-    
-    public Boolean ifCardActive(int n){
+
+    public Boolean ifCardActive(int n) {
         return markedCards[n];
     }
-    
-    public void setCardInPlay(int n, Boolean bool){
+
+    public void setCardInPlay(int n, Boolean bool) {
         markedCards[n] = bool;
     }
-    
-    public void connectCountries(Country c1, Country c2){
+
+    public void connectCountries(Country c1, Country c2) {
         c1.addAdjacentCountry(c2);
         c2.addAdjacentCountry(c1);
     }
-    
-    public ArrayList<Country> getAllCountries(){
+
+    public ArrayList<Country> getAllCountries() {
         return countries;
     }
-    
-    public Deck getDrawPile(){
+
+    public Deck getDrawPile() {
         return drawPile;
     }
-   
-    
-    private void createWorld(){
-        
+
+    public void setFunding(int funding) {
+        this.funding = funding;
+    }
+
+    public void setPrestige(int prestige) {
+        this.prestige = prestige;
     }
     
-    public void modifyPrestige(int change){
+    public int getFunding(){
+        return funding;
+    }
+    
+    public int getPrestige(){
+        return prestige;
+    }
+
+    public void modifyPrestige(int change) {
         prestige = prestige + change;
-        if (prestige < 1){
+        if (prestige < 1) {
             prestige = 1;
-        }
-        else if (prestige > 12){
+        } else if (prestige > 12) {
             prestige = 12;
         }
     }
-    
-    public void modifyFunding(int change){
+
+    public void modifyFunding(int change) {
         funding = funding + change;
-        if (funding < 1){
+        if (funding < 1) {
             funding = 1;
-        }
-        else if (funding > 9){
+        } else if (funding > 9) {
             funding = 9;
         }
     }
