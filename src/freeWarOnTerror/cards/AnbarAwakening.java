0c6bfc096d7c89719df6ec294abd3f55c9db1e12
Emil Istrup Karlsmose <emilistrupkarlsmose@gmail.com>
2014-11-01 21:55:43 +0100

    added card Anbar awakening - not fully done!

---
+++ src/freeWarOnTerror/cards/AnbarAwakening.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2014 Emil
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package freeWarOnTerror.cards;
+
+import static freeWarOnTerror.Game.getCountry;
+import freeWarOnTerror.abClasses.Card;
+import static freeWarOnTerror.helpers.CONSTANTS.IRAQ;
+import static freeWarOnTerror.helpers.CONSTANTS.SYRIA;
+
+/**
+ *
+ * @author Emil
+ */
+public class AnbarAwakening extends Card {
+    
+    public AnbarAwakening(){
+        super("Anbar Awakening", 2, 2, false);
+    }
+    
+    @Override
+    public Boolean getPlayable(){
+        return getCountry(IRAQ).hasTroops() || getCountry(SYRIA).hasTroops();
+    }
+    
+    @Override
+    public void play(){
+        //DEBUG TBD
+    }
+}
