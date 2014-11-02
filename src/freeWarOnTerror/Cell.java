9ef03a774b777ec1bff45fae78d0113070892290
Emil Istrup Karlsmose <emilistrupkarlsmose@gmail.com>
2014-11-02 14:42:30 +0100

    [Cell] overrides move()

--- src/freeWarOnTerror/Cell.java
+++ src/freeWarOnTerror/Cell.java
@@ -12,35 +12,42 @@ import freeWarOnTerror.abClasses.Moveable;
  *
  * @author Emil
  */
-public class Cell extends Moveable{
+public class Cell extends Moveable {
+
     private Boolean active = false;
     private boolean idle = true;
     private boolean backlash = false;
     
-    public Cell(Location l){
+    public Cell(Location l) {
         super(l);
     }
-
+    
+    @Override
+    public void move(Location l) {
+        setActive(false);
+        super.move(l);
+    }
+    
     public boolean isIdle() {
         return idle;
     }
-
+    
     public void setIdle(boolean idle) {
         this.idle = idle;
     }
-
-    public void setActive(Boolean active){
+    
+    public void setActive(Boolean active) {
         this.active = active;
     }
     
-    public Boolean getActive(){
+    public Boolean getActive() {
         return active;
     }
-
+    
     public boolean isBacklash() {
         return backlash;
     }
-
+    
     public void setBacklash(boolean backlash) {
         this.backlash = backlash;
     }
