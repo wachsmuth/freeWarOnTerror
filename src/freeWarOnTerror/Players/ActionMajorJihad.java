82ad4950359bac451b54ff7eff5dead91f136a6a
Emil Istrup Karlsmose <emilistrupkarlsmose@gmail.com>
2014-11-03 21:10:46 +0100

    new

---
+++ src/freeWarOnTerror/Players/ActionMajorJihad.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2014 Emil
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package freeWarOnTerror.Players;
+
+import static freeWarOnTerror.Game.getMuslimCountries;
+import freeWarOnTerror.MuslimCountry;
+import freeWarOnTerror.abClasses.Action;
+import freeWarOnTerror.abClasses.Card;
+import freeWarOnTerror.abClasses.Country;
+
+/**
+ *
+ * @author Emil
+ */
+public class ActionMajorJihad extends Action {
+    
+    public ActionMajorJihad(){
+        super("Use ops for Major Jihad");
+    }
+    
+    @Override
+    public boolean canDoAction(Card c){
+                for (Country country : getMuslimCountries()) {
+            MuslimCountry x = (MuslimCountry) country;
+            if (x.canMajorJihad(c.getOps())) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    @Override
+    public void performAction(Card c){
+        //DEBUG to do
+    }
+    
+}
