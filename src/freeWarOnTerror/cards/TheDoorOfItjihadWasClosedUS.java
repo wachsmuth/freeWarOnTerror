3b7daedc307c33be82cbb7697defe8b074fcfd30
Emil Istrup Karlsmose <emilistrupkarlsmose@gmail.com>
2014-11-24 04:27:18 +0100

    add card The Door of Itjihad was CLosed (US)

---
+++ src/freeWarOnTerror/cards/TheDoorOfItjihadWasClosedUS.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2014 Emil
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package freeWarOnTerror.cards;
+
+import static freeWarOnTerror.Game.getCurrentTurn;
+import freeWarOnTerror.abClasses.Card;
+import freeWarOnTerror.helpers.CardLookup;
+
+/**
+ *
+ * @author Emil
+ */
+public class TheDoorOfItjihadWasClosedUS extends Card {
+    
+    public TheDoorOfItjihadWasClosedUS(){
+        super("\"The Door of Itjihad Was Closed\"", 3, 2, false, false, CardLookup.THEDOOROFITJIHADWASCLOSEDUS);
+    }
+    
+    @Override
+    public void playEvent(){
+        getCurrentTurn().itjihad();
+    }
+}
